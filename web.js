ee2409cc7d8ec056393ed45dcf4c9f4796e2d0e1
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-04-25 15:32:01 +0530

    ✨ feat(web): add draw method

--- web.js
+++ web.js
@@ -11,6 +11,27 @@ const {
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+// Game loop draw
+const draw = () => {
+  // clear
+  ctx.fillStyle = "#232323";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // draw snake
+  ctx.fillStyle = "rgb(0,200,50)";
+  state.snake.map((p) => ctx.fillRect(x(p.x), y(p.y, x(1), y(1))));
+
+  // draw apple
+  ctx.fillStyle = "rgb(255,0,50)";
+  ctx.fillRect(x(state.apple.x), y(state.apple.y), x(1), y(1));
+
+  // add crash
+  if (state.snake.length === 0) {
+    ctx.fillStyle = "rgb(255,0,0)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+};
+
 // Mutable state
 let state = initialState();
 
